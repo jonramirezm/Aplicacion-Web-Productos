71702578f3967eccd6916c60826ad120cad8f4d8
Jonathan Ramirez <jonathan.ramirezm12@gmail.com>
2021-11-15 13:27:56 -0400

    Creacion de Estructura MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Aplicacion_Web_Productos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
